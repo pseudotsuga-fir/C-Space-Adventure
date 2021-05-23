8d365d4099cf00ad51dac00d864a80ee0a263ca5
pseudotsuga-fir <hepworta@oregonstate.edu>
2021-05-23 13:55:13 -0700

    Program given pseudofunctionality. Written monolithically
    main.c written to fake working functionality, however it does not retrieve any data from a given JSON file. The program has been written entirely inside the main function and needs refactoring

--- main.c
+++ main.c
@@ -1,8 +1,37 @@
 //C Space Adventure
 #include <stdio.h>
+#include <string.h>
+// #include <json-c/json.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-  printf("In main\n");
-  return 0;
+  // FILE *fp;
+  // char json_buffer[1024];
+  char name[120];
+  char yn_choice[4];
+  char user_planet[120];
+  printf("Welcome to the solar system.\nThere are 9 planets to explore.\n");
+  printf("What is your name? : ");
+  fgets(name, 120, stdin);
+  name[strcspn(name, "\n")] = 0;
+  printf("Welcome ");
+  puts(name);
+  printf("Would you like to travel to a random planet? (Y/N) : ");
+  fgets(yn_choice, 4, stdin);
+  yn_choice[strcspn(yn_choice, "\n")] = 0;
+  if(strcmp(yn_choice, "Y") == 0 || strcmp(yn_choice, "y") == 0){
+    printf("Let's go to Neptune\n");
+    printf("Neptune - A very cold planet, furthest from the sun.\n");
+  }
+  else if(strcmp(yn_choice, "N") == 0 || strcmp(yn_choice, "n") == 0){
+    printf("Where to instead? : ");
+    fgets(user_planet, 120, stdin);
+    user_planet[strcspn(user_planet, "\n")] = 0;
+    printf("Good choice. Let's go to Mars!\n");
+    printf("Mars - Known as the red planet.\n");
+
+  }
+  else{
+    printf("Sorry, I didn't understand.");
+  }
 }
