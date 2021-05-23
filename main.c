306b2a15966c4e6a89607a69a87dc7aeed4f1984
pseudotsuga-fir <hepworta@oregonstate.edu>
2021-05-23 14:54:17 -0700

    main.c refactored into functions
    main.c's main function broken up into several smaller functions for readability and reusability

--- main.c
+++ main.c
@@ -3,35 +3,63 @@
 #include <string.h>
 // #include <json-c/json.h>
 
-int main(int argc, char *argv[])
+void printIntro()
 {
-  // FILE *fp;
-  // char json_buffer[1024];
-  char name[120];
-  char yn_choice[4];
-  char user_planet[120];
   printf("Welcome to the solar system.\nThere are 9 planets to explore.\n");
-  printf("What is your name? : ");
-  fgets(name, 120, stdin);
-  name[strcspn(name, "\n")] = 0;
+}
+
+char * getInput(char prompt[120])
+{
+  static char input[120];
+  printf("%s", prompt);
+  fgets(input, 120, stdin);
+  input[strcspn(input, "\n")] = 0;
+  return input;
+}
+
+void printGreeting(char name[120])
+{
   printf("Welcome ");
-  puts(name);
-  printf("Would you like to travel to a random planet? (Y/N) : ");
-  fgets(yn_choice, 4, stdin);
-  yn_choice[strcspn(yn_choice, "\n")] = 0;
-  if(strcmp(yn_choice, "Y") == 0 || strcmp(yn_choice, "y") == 0){
-    printf("Let's go to Neptune\n");
-    printf("Neptune - A very cold planet, furthest from the sun.\n");
+  printf("%s", name);
+}
+
+int askRandomPlanet(char choice[120])
+{
+  //Function returns 1 if player chooses random planet, 0 if they choose not to go to a random planet.
+  if(strcmp(choice, "Y") == 0 || strcmp(choice, "y") == 0){
+    return 1;
   }
-  else if(strcmp(yn_choice, "N") == 0 || strcmp(yn_choice, "n") == 0){
-    printf("Where to instead? : ");
-    fgets(user_planet, 120, stdin);
-    user_planet[strcspn(user_planet, "\n")] = 0;
-    printf("Good choice. Let's go to Mars!\n");
-    printf("Mars - Known as the red planet.\n");
+  else if(strcmp(choice, "N") == 0 || strcmp(choice, "n") == 0){
+    return 0;
 
   }
   else{
     printf("Sorry, I didn't understand.");
+    askRandomPlanet(getInput("\nWould you like to travel to a random planet? (Y/N) : "));
+  }
+}
+
+void randomPlanet()
+{
+  printf("Let's go to Neptune\n");
+  printf("Neptune - A very cold planet, furthest from the sun.\n");
+}
+
+void playerPlanet(char choice[120]){
+    printf("Good choice. Let's go to Mars!\n");
+    printf("Mars - Known as the red planet.\n");
+}
+
+int main(int argc, char *argv[])
+{
+  // FILE *fp;
+  // char json_buffer[1024];
+  printIntro();
+  printGreeting(getInput("What is your name? : "));
+  if(askRandomPlanet(getInput("\nWould you like to travel to a random planet? (Y/N) : "))){
+    randomPlanet();
+  }
+  else{
+    playerPlanet(getInput("Where to instead? : "));
   }
 }
