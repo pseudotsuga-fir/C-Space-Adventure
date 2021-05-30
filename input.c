9b4ea0df7e40c6e52af1f5ca28de295f7a8b0f66
pseudotsuga-fir <hepworta@oregonstate.edu>
2021-05-30 13:08:18 -0700

    Function header files added

---
+++ input.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include "input.h"
+
+void printIntro()
+{
+  printf("Welcome to the solar system.\nThere are 9 planets to explore.\n");
+}
+
+char * getInput(char prompt[120])
+{
+  static char input[120];
+  printf("%s", prompt);
+  fgets(input, 120, stdin);
+  input[strcspn(input, "\n")] = 0;
+  return input;
+}
+
+void printGreeting(char name[120])
+{
+  printf("Welcome ");
+  printf("%s", name);
+}
+
+int askRandomPlanet(char choice[120])
+{
+  //Function returns 1 if player chooses random planet, 0 if they choose not to go to a random planet.
+  if(strcmp(choice, "Y") == 0 || strcmp(choice, "y") == 0){
+    return 1;
+  }
+  else if(strcmp(choice, "N") == 0 || strcmp(choice, "n") == 0){
+    return 0;
+
+  }
+  else{
+    printf("Sorry, I didn't understand.");
+    askRandomPlanet(getInput("\nWould you like to travel to a random planet? (Y/N) : "));
+  }
+}
+
+void randomPlanet()
+{
+  printf("Let's go to Neptune\n");
+  printf("Neptune - A very cold planet, furthest from the sun.\n");
+}
+
+void playerPlanet(char choice[120])
+{
+    printf("Good choice. Let's go to Mars!\n");
+    printf("Mars - Known as the red planet.\n");
+}
---
+++ input.h
@@ -0,0 +1,11 @@
+void printIntro();
+
+char * getInput(char prompt[120]);
+
+void printGreeting(char name[120]);
+
+int askRandomPlanet(char choice[120]);
+
+void randomPlanet();
+
+void playerPlanet(char choice[120]);
